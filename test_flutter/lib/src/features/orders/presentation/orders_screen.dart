0897cbc14b89b97463ba6775df6455e1bd3a329b
Raden6 <naderinsa@gmail.com>
2024-06-04 19:37:31 +0200

    OrdersScreen: support navigating to OrderScreen
    An OrderScreen is created which will serve for displaying detais about
    an order. The OrderScreen is set as a subroute to the OrdersScreen, and
    therefore it can be pushed into the route stack by tapping on the order
    cards.

--- test_flutter/lib/src/features/orders/presentation/orders_screen.dart
+++ test_flutter/lib/src/features/orders/presentation/orders_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:test_flutter/src/features/orders/data/fake_orders_repository.dart';
+import 'package:test_flutter/src/routing/app_router.dart';
 
 class OrdersScreen extends ConsumerWidget {
   const OrdersScreen({super.key});
@@ -27,38 +29,43 @@ class OrdersScreen extends ConsumerWidget {
                     (previousValue, element) => previousValue + element.price,
                   );
 
-              return Card(
-                child: Row(
-                  children: [
-                    Container(
-                      width: 48,
-                      height: 48,
-                      color: Colors.red,
-                      child: Text(
-                        orders[index].table,
-                        textAlign: TextAlign.center,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 24,
-                          color: Colors.white,
+              return GestureDetector(
+                onTap: () {
+                  context.pushNamed(AppRoute.order.name);
+                },
+                child: Card(
+                  child: Row(
+                    children: [
+                      Container(
+                        width: 48,
+                        height: 48,
+                        color: Colors.red,
+                        child: Text(
+                          orders[index].table,
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24,
+                            color: Colors.white,
+                          ),
                         ),
                       ),
-                    ),
-                    Column(
-                      children: [
-                        Row(
-                          children: [
-                            const Icon(Icons.face),
-                            Text('${orders[index].guests}'),
-                          ],
-                        ),
-                        if (orders[index].date != null)
-                          Text(orders[index].date.toString()),
-                      ],
-                    ),
-                    const SizedBox(width: 24),
-                    Text('$total')
-                  ],
+                      Column(
+                        children: [
+                          Row(
+                            children: [
+                              const Icon(Icons.face),
+                              Text('${orders[index].guests}'),
+                            ],
+                          ),
+                          if (orders[index].date != null)
+                            Text(orders[index].date.toString()),
+                        ],
+                      ),
+                      const SizedBox(width: 24),
+                      Text('$total')
+                    ],
+                  ),
                 ),
               );
             },
--- test_flutter/lib/src/routing/app_router.dart
+++ test_flutter/lib/src/routing/app_router.dart
@@ -1,8 +1,12 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:test_flutter/src/features/orders/presentation/order_screen.dart';
 import 'package:test_flutter/src/features/orders/presentation/orders_screen.dart';
 
-enum AppRoute { orders }
+enum AppRoute {
+  orders,
+  order,
+}
 
 final goRouterProvider = Provider<GoRouter>((ref) {
   return GoRouter(
@@ -12,6 +16,13 @@ final goRouterProvider = Provider<GoRouter>((ref) {
         path: '/orders',
         name: AppRoute.orders.name,
         builder: (context, state) => const OrdersScreen(),
+        routes: [
+          GoRoute(
+            path: 'order',
+            name: AppRoute.order.name,
+            builder: (context, state) => const OrderScreen(),
+          ),
+        ],
       ),
     ],
   );
