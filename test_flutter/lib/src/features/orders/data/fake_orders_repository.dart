b427346ae8749c77faf4a3aa3d994c475c2e374d
Raden6 <naderinsa@gmail.com>
2024-06-04 13:36:41 +0200

    Emulate fetching data w/ FakeOrdersRepository
    A FakeOrdersRepository is created to emulate fetching the orders data
    from an access point. For now, this class will serve for sketching the
    UI logic, and can later be used for writing tests.
    
    The riverpod v2.3.7 package was installed, so that the orders data could
    be exposed to the widgets via a FutureProvider.

---
+++ test_flutter/lib/src/features/orders/data/fake_orders_repository.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:test_flutter/src/features/orders/domain/item.dart';
+import 'package:test_flutter/src/features/orders/domain/order.dart';
+
+class FakeOrdersRepository {
+  final _orders = <Order>[
+    Order(
+      id: 1,
+      table: '1',
+      guests: 2,
+      date: null,
+      items: [
+        Item(
+          id: 1,
+          name: 'one',
+          price: 1,
+          currency: '€',
+          color: Colors.red,
+        ),
+      ],
+    ),
+    Order(
+      id: 2,
+      table: '2',
+      guests: 3,
+      date: null,
+      items: [
+        Item(
+          id: 1,
+          name: 'one',
+          price: 1,
+          currency: '€',
+          color: Colors.red,
+        ),
+      ],
+    ),
+  ];
+
+  Future<List<Order>> _fetchOrders() async {
+    // emulate network request
+    await Future.delayed(const Duration(seconds: 2));
+
+    return _orders;
+  }
+}
+
+final fakeOrdersRepositoryProvider = Provider<FakeOrdersRepository>((ref) {
+  return FakeOrdersRepository();
+});
+
+final fakeOrdersFutureProvider = FutureProvider<List<Order>>((ref) async {
+  final fakeOrdersRepository = ref.watch(fakeOrdersRepositoryProvider);
+  return fakeOrdersRepository._fetchOrders();
+});
