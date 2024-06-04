ed6c37f6d7c7aadddbb168c3c568ef11267c6486
Raden6 <naderinsa@gmail.com>
2024-06-04 15:21:42 +0200

    OrdersScreen: display number of orders in AppBar
    The OrdersScreen is now wrapped with a Scaffold with an AppBar
    displaying the number of orders.

--- test_flutter/lib/src/features/orders/presentation/orders_screen.dart
+++ test_flutter/lib/src/features/orders/presentation/orders_screen.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:test_flutter/src/features/orders/data/fake_orders_repository.dart';
 
-class OrdersScreen extends StatelessWidget {
+class OrdersScreen extends ConsumerWidget {
   const OrdersScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final ordersValue = ref.watch(fakeOrdersFutureProvider);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: ordersValue.when(
+          data: (orders) => Text('${orders.length} commandes'),
+          error: (e, st) => Center(child: Text(e.toString())),
+          loading: () => const Center(child: CircularProgressIndicator()),
+        ),
+      ),
+      body: const Placeholder(),
+    );
   }
 }
